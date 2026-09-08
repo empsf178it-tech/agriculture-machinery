8bfb76f791e3f31e04d935bb6a925479c43ff6a7
empsf178it-tech <empsf178@gmail.com>
2026-09-08 10:15:33 +0530

    Fix GitHub Pages deployment blank screen with relative base path and HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './context/LanguageContext';
 import ScrollToTop from './components/ScrollToTop';
 import Navbar from './components/Navbar';
